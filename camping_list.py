efc77f4add106cc5f2925b419056729a56daf348
Jackie <jackiekellett@tuta.io>
2022-09-26 18:33:39 -0400

    Update camping_list.py

--- camping_list.py
+++ camping_list.py
@@ -1,24 +1,11 @@
 #    Camping List
 # ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
 
-camping_stuff = "tent, sleeping bags, water, marshmallows, chocolate, graham crackers, lighter, lantern, mug"
-
-print(camping_stuff)
-print(type(camping_stuff))
-
-camping_list = ['tent', 'sleeping bag', 'sleeping bag', 'water', 'marshmallows', 'chocolate', 'graham crackers', 'lighter', 'lantern', 'mug']
-
-print(camping_list)
-print(type(camping_list))
+supplies = ['tent', 'sleeping bag', 'sleeping bag', 'water', 'marshmallows', 'chocolate', 'graham crackers', 'lighter', 'lantern', 'mug']
 
 # Make a campsite with the Name, Lot number, Temperature, Dangerous?
 camp_site = ['Mammoth Cave', 200, 76.4, False]
 
-print(camp_site)
-print(type(camp_site))
+supplies.append('toilet paper')
 
-# Lists are ordered / sequential / indexed
-# NOTE: the first item on a list is rank 0; hence, if we want the fifth item, we must specify [4]
-item_5 = camping_list[4]
-
-print(item_5)
+print(supplies)
