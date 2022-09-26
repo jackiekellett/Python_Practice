17d08e99d5fabab46b3d250d19139a27efb42605
Jackie <jackiekellett@tuta.io>
2022-09-26 17:09:53 -0400

    Add "if" statement, prevent banned customer

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 #  Automated Barista
 # ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
 
+# Price variable
+price = 5
+# Tax variable
+tax = 0
+
+# Menu string
+menu = "Latte, Coffee, Espresso, Americano"
+
 # Welcome the customer
 print("\nHowdy! Welcome to the Computer Coffee House!")
 
 # Name variable / Get the customer's name
 name = input("What is your name?\n")
 
-# Menu string
-menu = "Latte, Coffee, Espresso, Americano"
-
-
-# Order variable / Get the customer's order
-order = input("Thanks for coming in today, " + name + "! What are you having today? Here's what we have on the menu:\n" + menu + "\n")
-
-# Price variable
-price = 5
-
-# Amount variable / Get the number of drinks
-amount = input("How many " + order + "s would you like?\n")
-
-# Tax variable
-tax = 0
-
+# Refuse service to banned customers
+if name == "Quinn":
+    print("You're not welcome here, " + name + "! Get out!!!")
+else:
+# Get the customer's order
+    order = input("Thanks for coming in today, " + name + "! What are you having today? Here's what we have on the menu:\n" + menu + "\n")
+# Get drink amount
+    amount = input("How many " + order + "s would you like?\n")
 # Calculate the total
-total = (1 + tax) * (price * int(amount))
-
+    total = (1 + tax) * (price * int(amount))
 # Tell the customer the total
-print("Alright, your total today is $" + str(total) + ".\n")
+    print("Alright, " + name + ", your total today is $" + str(total) + ".\n")
