db721bcc7cf388289849830056cf8f16bd8299c2
Jackie <jackiekellett@tuta.io>
2022-09-26 17:14:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ name = input("What is your name?\n")
 # Refuse service to banned customers
 if name == "Quinn":
     print("You're not welcome here, " + name + "! Get out!!!")
-else:
+    exit()
+
 # Get the customer's order
-    order = input("Thanks for coming in today, " + name + "! What are you having today? Here's what we have on the menu:\n" + menu + "\n")
+order = input("Thanks for coming in today, " + name + "! What are you having today? Here's what we have on the menu:\n" + menu + "\n")
 # Get drink amount
-    amount = input("How many " + order + "s would you like?\n")
+amount = input("How many " + order + "s would you like?\n")
 # Calculate the total
-    total = (1 + tax) * (price * int(amount))
+total = (1 + tax) * (price * int(amount))
 # Tell the customer the total
-    print("Alright, " + name + ", your total today is $" + str(total) + ".\n")
+print("Alright, " + name + ", your total today is: $" + str(total))
