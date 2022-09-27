1c815ba95fa403dba2fd1139b8be67f6f6350010
Jackie <jackiekellett@tuta.io>
2022-09-27 07:37:19 -0400

    Using a class

--- rock_paper_scissors_lizard_spock.py
+++ rock_paper_scissors_lizard_spock.py
@@ -1,11 +1,22 @@
-import numpy as np
-import pandas as pd
-
 import random
 
+class Choice:
+    def __init__(self, name, ab, ftw):
+        self.name = name
+        self.abbrev = ab
+        self.beats = ftw
+
+c1 = Choice('Rock', 'R', ['S', 'L'])
+c2 = Choice('Paper', 'P', ['R', 'K'])
+c3 = Choice('Scissors', 'S', ['P', 'L'])
+c4 = Choice('Lizard', 'L', ['P', 'K'])
+c5 = Choice('Spock', 'K', ['R', 'S'])
+
+options = [c1.abbrev, c2.abbrev, c3.abbrev, c4.abbrev, c5.abbrev]
+
 def play():
-    user = input('Let\'s play Rock, Paper, Scissors, Lizard, Spock!\nWhat\'s your choice?\n     [R] for Rock, [P] for Paper, [S] for Scissors, [L] for Lizard, [K] for Spock\n').upper()
-    computer = random.choice(['R', 'P', 'S', 'L', 'K'])
+    user = input(f'Let\'s play {c1.name}, {c2.name}, {c3.name}, {c4.name}, {c5.name}!\nWhat\'s your choice?\n     [{c1.abbrev}] for {c1.name}, [{c2.abbrev}] for {c2.name}, [{c3.abbrev}] for {c3.name}, [{c4.abbrev}] for {c4.name}, [{c5.abbrev}] for {c5.name}\n').upper()
+    computer = random.choice(options)
     print(f'Computer plays {computer}.\n')
     if user == computer:
         return 'It\'s a tie. You both live to fight another day...'
@@ -14,17 +25,13 @@ def play():
     return 'FATALITY\nYou lose.'
 
 def is_win(player, opponent):
-    # return true if player wins
-    # R > S, L
-    # P > R, K
-    # S > P, L
-    # L > P, K
-    # K > R, S
-    if (player == 'R' and opponent in ['S', 'L'])\
-            or (player == 'P' and opponent in ['R', 'K'])\
-            or (player == 'S' and opponent in ['P', 'L'])\
-            or (player == 'L' and opponent in ['P', 'K'])\
-            or (player == 'K' and opponent in ['R', 'S']):
+    if (player == c1.abbrev and opponent in c1.beats)\
+            or (player == c2.abbrev and opponent in c2.beats)\
+            or (player == c3.abbrev and opponent in c3.beats)\
+            or (player == c4.abbrev and opponent in c4.beats)\
+            or (player == c5.abbrev and opponent in c5.beats):
         return True
+    else:
+        return False
 
 print(play())
